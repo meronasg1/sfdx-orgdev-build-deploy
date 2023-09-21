2ee164ec7c1970c7ad4b296c59db3290ddc0b483
meronasg1 <76233217+meronasg1@users.noreply.github.com>
2023-09-21 17:19:59 -0400

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -13284,15 +13284,34 @@ module.exports.run = function(command, args, workingFolder = null) {
 const core = __webpack_require__(2186)
 const execCommand = __webpack_require__(5505);
 
-var fnInstallSFDX = function(){
-	core.info('=== Downloading and installing SFDX cli ===');
-	execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sf/channels/stable/sf-linux-x64.tar.xz']);
-	execCommand.run('mkdir', ['-p', 'sfdx-cli']);
-	execCommand.run('tar', ['xJf', 'sf-linux-x64.tar.xz', '-C', 'sfdx-cli', '--strip-components', '1']);
-	execCommand.run('./sfdx-cli/install', []);
+const core = __webpack_require__(2186);
+const execCommand = __webpack_require__(5505);
 
-    core.info('=== SFDX cli installed ===');
+var fnInstallSFDX = function () {
+    try {
+        core.info('=== Downloading SFDX cli ===');
+        execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sf/channels/stable/sf-linux-x64.tar.xz']);
+        
+        core.info('=== Creating Directory ===');
+        execCommand.run('mkdir', ['-p', 'sfdx-cli']);
+        
+        core.info('=== Extracting Tar Archive ===');
+        execCommand.run('tar', ['xJf', 'sf-linux-x64.tar.xz', '-C', 'sfdx-cli', '--strip-components', '1']);
+        
+        core.info('=== Running Install Script ===');
+        execCommand.run('./sfdx-cli/install', []);
+
+        core.info('=== SFDX cli installed ===');
+    } catch (error) {
+        // Log and handle errors for each command separately
+        if (error.message) {
+            core.error(`An error occurred: ${error.message}`);
+        }
+        
+        // Handle other specific errors here if needed
+    }
 };
+
 
 module.exports.install = function(command, args) {
     //Installs Salesforce DX CLI
