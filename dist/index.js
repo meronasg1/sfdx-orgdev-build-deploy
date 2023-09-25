42801f582d37d3ad949ff7376a84e21fff2e7c9a
meronasg1 <76233217+meronasg1@users.noreply.github.com>
2023-09-25 09:36:51 -0400

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -13370,7 +13370,7 @@ let login = function (cert, login){
     core.info('==== Authenticating in the target org');
     const instanceurl = login.orgType === 'sandbox' ? 'https://test.salesforce.com' : 'https://login.salesforce.com';
     core.info('Instance URL: ' + instanceurl);
-    execCommand.run('sfdx', ['force:auth:jwt:grant', '--instanceurl', instanceurl, '--clientid', login.clientId, '--jwtkeyfile', 'server.key', '--username', login.username, '--setalias', 'sfdc']);
+    execCommand.run('sfdx-cli/bin/sfdx', ['force:auth:jwt:grant', '--instanceurl', instanceurl, '--clientid', login.clientId, '--jwtkeyfile', 'server.key', '--username', login.username, '--setalias', 'sfdc']);
 };
 
 let deploy = function (deploy){
